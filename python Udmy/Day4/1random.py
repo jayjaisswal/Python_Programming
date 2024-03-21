4c3b38c848e50dde1b13161e82c1a6fbc27411e6
Jay kumar <kumarprince13833@gmail.com>
2024-03-22 00:34:27 +0530

    random_Function

---
+++ python Udmy/Day4/1random.py
@@ -0,0 +1,13 @@
+import random
+import my_module
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+
+# print(my_module.pi)
+
+random_float = random.random()
+new_ran = random_float*5 + 1
+int_ran = round (new_ran, 0)
+print(int(int_ran))
