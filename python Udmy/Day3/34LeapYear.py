c0a5ef14ccb044ab5177e47a2bc21c93d6b4b3ee
Jay kumar <kumarprince13833@gmail.com>
2024-03-21 09:23:09 +0530

    leapyear

--- python Udmy/Day3/34LeapYear.py
+++ python Udmy/Day3/34LeapYear.py
@@ -1,7 +1,7 @@
 year = int(input())
 if year % 4 == 0:
     if year % 100 == 0:
-        if year % 4 == 0:
+        if year % 400 == 0:
             print("Leap year")
         else:
             print("Not Leap year")
